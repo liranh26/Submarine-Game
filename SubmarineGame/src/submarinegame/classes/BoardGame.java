9ba32c6cb544ce3b3fe26a303ae8f86ae37ef10c
Liran Hadad <liranh6@gmail.com>
2022-04-20 15:20:31 +0300

    adding initialezeBoard and consctroctor

--- SubmarineGame/src/submarinegame/classes/BoardGame.java
+++ SubmarineGame/src/submarinegame/classes/BoardGame.java
@@ -3,9 +3,25 @@ package submarinegame.classes;
 public class BoardGame {
 	public int rows;
 	public int cols;
+	private char[][] board;
+	
+	public BoardGame() {
+		board = new char[rows][cols];
+		initializeBoard();
+	}
+	
+	
+	private void initializeBoard() {
+		for(int i=0; i< rows; i++) {
+			for(int j=0; j<cols; j++) {
+				board[i][j] = ' ';
+			}
+		}
+	}
+	
 	
 	public void setSubmarine() {
-		System.out.println("hello");
+		
 		
 	}
 }
