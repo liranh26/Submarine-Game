24c18318d0b2c5cf3daf5058dccf8905da21433d
roteml8 <missroteml@gmail.com>
2022-04-20 15:24:09 +0300

    add play

--- SubmarineGame/src/submarinegame/classes/Game.java
+++ SubmarineGame/src/submarinegame/classes/Game.java
@@ -4,11 +4,18 @@ public class Game {
 
 	protected int points;
 	protected int guesses;
+	protected int hits;
 	protected BoardGame board;
 	
 	public Game()
 	{
 		this.points = 1000;
 		this.guesses = 100;
+		this.hits = 0;
+	}
+	
+	public void play()
+	{
+		while (guesses > 0 && points > 0 )
 	}
 }
