e18e01b035fab201f71f18b7ecfa974d7af4f3cd
roteml8 <missroteml@gmail.com>
2022-04-20 15:09:02 +0300

    add game class

---
+++ SubmarineGame/src/submarinegame/classes/Game.java
@@ -0,0 +1,14 @@
+package submarinegame.classes;
+
+public class Game {
+
+	protected int points;
+	protected int guesses;
+	protected BoardGame board;
+	
+	public Game()
+	{
+		this.points = 1000;
+		this.guesses = 100;
+	}
+}
