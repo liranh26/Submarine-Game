f2e07fd9cd52b7e4bb7cc365213405d766f4ea6c
Liran Hadad <liranh6@gmail.com>
2022-04-20 15:41:34 +0300

    adding frame to board print

--- SubmarineGame/src/submarinegame/classes/BoardGame.java
+++ SubmarineGame/src/submarinegame/classes/BoardGame.java
@@ -4,34 +4,38 @@ public class BoardGame {
 	public int rows;
 	public int cols;
 	private char[][] board;
-	
+
 	public BoardGame() {
 		board = new char[rows][cols];
 		initializeBoard();
 	}
-	
-	
+
 	private void initializeBoard() {
-		for(int i=0; i< rows; i++) {
-			for(int j=0; j<cols; j++) {
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
 				board[i][j] = ' ';
 			}
 		}
 	}
-	
-	
+
 	public void setSubmarine() {
-		
-		
+
 	}
-	
-	public void printBoard()
-	{
-		for (int i = 0; i < board.length; i++) {
-			for (int j = 0; j < board[i].length; j++) {
-				System.out.println(board[i][j]);
+
+	public void printBoard() {
+		for (int i = 0; i < board.length + 2; i++) {
+			if (i == 0 || i == board.length - 1)
+				System.out.println("*");
+			else {
+				for (int j = 0; j < board[i].length + 2; j++) {
+					if (j == 0 || j == board[i].length - 1)
+						System.out.println("*");
+					else {
+						System.out.println(board[i][j]);
+					}
+				}
 			}
-		System.out.println();
+			System.out.println();
 		}
 	}
 }
--- SubmarineGame/src/submarinegame/classes/Game.java
+++ SubmarineGame/src/submarinegame/classes/Game.java
@@ -31,4 +31,6 @@ public class Game {
 			//checkGuess(x,y)
 		}
 	}
+	
+	
 }
